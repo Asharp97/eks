5c01c40f4d5480bd94f9a238235e6562938ac611
Ali Elsayed <64378778+Asharp97@users.noreply.github.com>
2023-09-22 15:00:36 +0300

    initial

---
+++ app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  font-family: 'Inter', sans-serif;
+
+  body {
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    background-color: #fff;
+    overflow-x: hidden;
+  }
+}
+</style>
